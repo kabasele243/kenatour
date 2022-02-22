f1aac84e27249fd73c039d45baf4c2ee63a03430
Franck Kabasele <kabasele467@gmail.com>
2022-02-22 14:50:04 -0500

    set up signin and signup page

--- client/src/pages/login.page.jsx
+++ client/src/pages/login.page.jsx
@@ -1,26 +1,48 @@
-function login(){
+import { useState } from 'react';
+import Header from '../components/header/header.component';
+
+export default function Login(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        console.log(email, password)
+        setEmail('')
+        setPassword('')
+    
+      }
+
     return (
-        <div>
-<h1>Login</h1>
+        <div >
+            <Header/>
+            <div className="basic-form">
+                <h2 className="heading-secondary ma-bt-lg"> Log into your account</h2>
+                <form className="form form--login">
+                    <div className="form__group">
+                        <label htmlFor="" className="form__label">Email Address</label>
+                        <input 
+                            type="email" 
+                            placeholder="Email"
+                            className="form__input"
+                            onChange={(e) => setEmail(e.target.value)}/>
+                    </div>
+                    <div className="form__group ma-bt-md">
+                        <label htmlFor="" className="form__label">Password</label>
+                        <input 
+                            type="password" 
+                            placeholder="Password" 
+                            className="form__input"
+                            onChange={(e) => setPassword(e.target.value)}/>
+                    </div>
+
+                    <div className="form__group">
+                        <button className="btn btn--green" onClick={handleLogin}>Login</button>
+                    </div>
+
+                </form>
+            </div>
+            
         </div>
     )
 }
-
-export default login;
-
-
-// extends base
-
-// block content
-//   main.main
-//     .basic-form
-//       h2.heading-secondary.ma-bt-lg Log into your account
-//       form.form.form--login
-//         .form__group
-//           label.form__label(for='email') Email address
-//           input#email.form__input(type='email', placeholder='you@example.com', required)
-//         .form__group.ma-bt-md
-//           label.form__label(for='password') Password
-//           input#password.form__input(type='password', placeholder='••••••••', required, minlength='8')
-//         .form__group
-//           button.btn.btn--green Login
--- client/src/pages/signup.page.jsx
+++ client/src/pages/signup.page.jsx
@@ -1,7 +1,65 @@
+import { useState } from 'react';
+
 function Signup() {
+    const [name, setName]=useState("");
+    const [email, setEmail]=useState("");
+    const [password, setPassword]=useState("");
+    const [passwordConfirm, setPasswordConfirm]= useState("");
+
+    const handleSignup = (e) => {
+        e.preventDefault();
+        console.log({
+            name,
+            email,
+            password,
+            passwordConfirm
+        })
+    }
+
     return(
-        <div>
-            <h1>Sign up</h1>
+        <div className="main">
+            <div className="basic-form">
+                <h2 className="heading-secondary ma-bt-lg"> Sign up</h2>
+                <form className="form form--login">
+                    <div className="form__group">
+                        <label htmlFor="" className="form__label">User Name</label>
+                        <input 
+                            type="tect" 
+                            placeholder="User Name"
+                            className="form__input"
+                            onChange={(e) => setName(e.target.value)}/>
+                    </div>
+                    <div className="form__group">
+                        <label htmlFor="" className="form__label">Email Address</label>
+                        <input 
+                            type="email" 
+                            placeholder="Email"
+                            className="form__input"
+                            onChange={(e) => setEmail(e.target.value)}/>
+                    </div>
+                    <div className="form__group ma-bt-md">
+                        <label htmlFor="" className="form__label">Password</label>
+                        <input 
+                            type="password" 
+                            placeholder="Password" 
+                            className="form__input"
+                            onChange={(e) => setPassword(e.target.value)}/>
+                    </div>
+                    <div className="form__group ma-bt-md">
+                        <label htmlFor="" className="form__label">Password Confirmation</label>
+                        <input 
+                            type="password" 
+                            placeholder="Password" 
+                            className="form__input"
+                            onChange={(e) => setPasswordConfirm(e.target.value)}/>
+                    </div>
+
+                    <div className="form__group">
+                        <button className="btn btn--green" onClick={handleSignup}>Login</button>
+                    </div>
+
+                </form>
+            </div>
         </div>
     )
 }
